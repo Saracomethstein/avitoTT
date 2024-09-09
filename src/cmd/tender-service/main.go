ed7fec09afe72474ec04988a4910893a507c4d30
Adel Sevastianov <102406080+Saracomethstein@users.noreply.github.com>
2024-09-09 12:55:04 +0300

    edit config and ping function

--- src/cmd/tender-service/main.go
+++ src/cmd/tender-service/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	router := gin.Default()
+	router.GET("/api/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ok",
+		})
+	})
+
+	serverAddress := os.Getenv("SERVER_ADDRESS")
+	router.Run(serverAddress)
 }
