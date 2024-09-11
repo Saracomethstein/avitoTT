9d163737040facbf04e1e361e408116c8e05a319
Adel Sevastianov <102406080+Saracomethstein@users.noreply.github.com>
2024-09-11 13:47:46 +0300

    up: init db in postgres

---
+++ db/init/init.sql
@@ -0,0 +1,24 @@
+-- Tables for database tender-service.
+
+-- You can edit config in .env file in root.
+
+CREATE TYPE organization_type AS ENUM (
+    'IE',
+    'LLC',
+    'JSC'
+);
+
+CREATE TABLE organization (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    type organization_type,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE organization_responsible (
+    id SERIAL PRIMARY KEY,
+    organization_id INT REFERENCES organization(id) ON DELETE CASCADE,
+    user_id INT REFERENCES employee(id) ON DELETE CASCADE
+);
